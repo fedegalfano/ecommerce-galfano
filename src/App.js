e7ecc8b7755154d551b73e48a1b02a0bbe71b76a
Fede Galfano <fedegalfano@gmail.com>
2022-08-17 15:14:14 -0300

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ function ColorSchemesExample() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">VachPalm</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#home">Nuestra Historia</Nav.Link>
+            <Nav.Link href="#home">Productos</Nav.Link>
           </Nav>
+        </Container>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="/logo192.png"
+              width="30"
+              height="30"
+              className="d-inline-block"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </>
