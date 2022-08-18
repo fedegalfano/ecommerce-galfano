fc5dc905dd536e789056acef6652964b3ae13fb3
Fede Galfano <fedegalfano@gmail.com>
2022-08-18 10:32:44 -0300

    componente navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import NavBar from './components/NavBar';
 
-function ColorSchemesExample() {
+function App() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">VachPalm</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#home">Nosotros</Nav.Link>
-            <Nav.Link href="#home">Productos</Nav.Link>
-          </Nav>
-        </Container>
-        <Container className="justify-content-end">
-          <Navbar.Brand href="#home">
-            <img
-              src="/palma.png"
-              width="30"
-              height="30"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
+    <NavBar />
   );
 }
-
-export default ColorSchemesExample;
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function ColorSchemesExample() {
+function NavBar() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">VachPalm</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#home">Nosotros</Nav.Link>
+            <Nav.Link href="#home">Productos</Nav.Link>
           </Nav>
+        </Container>
+        <Container className="justify-content-end">
+          <Navbar.Brand href="#home">
+            <img
+              src="/palma.png"
+              width="30"
+              height="30"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </>
   );
 }
 
-export default ColorSchemesExample;
+export default NavBar;
